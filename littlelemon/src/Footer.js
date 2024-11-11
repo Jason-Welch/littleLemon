f1e660b4ba4a219c614cbcd9db62496f32959479
Aung Zin Tun <azt7324@gmail.com>
2024-11-11 12:03:17 +0630

    first step

---
+++ littlelemon/src/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <p>© 2023 Little Lemon. All Rights Reserved.</p>
+            <ul>
+                <li><a href="#privacy">Privacy Policy</a></li>
+                <li><a href="#terms">Terms of Service</a></li>
+            </ul>
+        </footer>
+    );
+}
+export default Footer;  
---
+++ littlelemon/src/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <header>
+            <img src = "./Logo.jpg" alt = "Little Lemon Logo" />
+        </header>
+    );
+}
+export default Header;  
